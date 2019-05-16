1dd5c9b51f12859cc2f0fa93cfe7bc781f1288f9
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-05-16 22:29:48 +0530

    routing 14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestomonialComponent } from './testomonial/testomonial.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { SocioFootComponent } from './socio-foot/socio-foot.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HeaderComponent },
+  { path: 'About', component: IntroComponent },
+  { path: 'Gallery', component: GalleryComponent },
+  { path: 'Services', component: ContentComponent },
+  { path: 'Testimonials', component: TestomonialComponent },
+  { path: 'Clients', component: ClientsComponent },
+  { path: 'Pricing', component: PricingComponent }
+];
+
+
+
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
