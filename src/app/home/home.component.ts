c1d5aaf786ad85f8076fa7c9c24f197493d8fe66
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2020-01-07 22:43:16 +0530

    home page updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { IImage } from '../IImage';
+import { Component, OnInit } from "@angular/core";
+import { IImage } from "../IImage";
 declare const myTest: any;
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
   public imageSources;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.imageSources = [
-      '../assets/images/softdev.jpg',
-      '../assets/images/agile-software.jpg',
-      '../assets/images/dev.jpg',
-      '../assets/images/com.jpg',
+      "../assets/images/softdev.jpg",
+      "../assets/images/pro.jpg",
+      "../assets/images/agile-software.jpg",
+      "../assets/images/full.jpg",
+      "../assets/images/dev.jpg",
+      "../assets/images/com.jpg"
     ];
   }
-
 }
